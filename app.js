51dae95bc46b621f0a51a728adf85a6dcb7fca58
David Woolf <davidjohnwoolf@gmail.com>
2015-10-25 13:58:03 -0700

    add mongodb and first model

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/routes');
 
@@ -12,6 +13,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+// database
+mongoose.connect('mongodb://localhost/professionQuest');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
