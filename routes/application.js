c7eb447ccfda1d89fcab7ade4dfdb5ad15c81150
Stephen Printup <stephen.printup@gmail.com>
2015-11-02 20:54:26 -0800

    pulled multiple apis

--- routes/application.js
+++ routes/application.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var bodyParser = require('body-parser');
-var request = require('request');
+var request = require('request-promise');
+var Promise = require("bluebird");
 
 router.use(bodyParser.urlencoded({ extended: false }));
 
@@ -16,54 +17,47 @@ function requireSession(req, res, next) {
 
 // Request
 router.get('/request/:q', function (req, res) {
-  // var searchResultsArray = [];
-  //dice
-	// request('http://service.dice.com/api/rest/jobsearch/v1/simple.json?text=' + req.params.q, function (error, response, body) {
-	//   if (!error && response.statusCode == 200) {
-	//     var result = JSON.parse(body);
- //      // res.send(result.resultItemList);
- //      searchResultsArray.concat(result.resultItemList);
-	//   }
- //  });
+  
+  function parsingToJSON(nonParsedData){
+    return JSON.parse(nonParsedData)
+  }
 
-  //github 
-  request('https://jobs.github.com/positions.json?search=code&page=1', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var result = JSON.parse(body);
-        console.log(result[1]);
-//[]  pull keys we want
-//[]  change keys to keys we want
-//[]  add to array
-//[]  sort into order we want
+  function githubTransformation(githubData){
+    return githubData.map(function(item){
+      var posting = {
+        title : item.title,
+        company : item.company,
+        postDate : new Date(item.created_at),
+        linkToSource : item.url
+      };
+      return posting;
+    })
+  }
 
+  function diceTransformation(diceData){
+    console.log(diceData)
+    return diceData.resultItemList.map(function(item){
+      var posting = {
+        title : item.jobTitle,
+        company : item.company,
+        postDate : new Date(item.date),
+        linkToSource : item.detailUrl
+      };
+      return posting;
+    })
+  }
 
-        // res.send(result);
-        // searchResultsArray.concat(result);
-    }
+  var github = request('https://jobs.github.com/positions.json?search=code&page=1')
+    .then(parsingToJSON)
+    .then(githubTransformation);
+  var dice = request('http://service.dice.com/api/rest/jobsearch/v1/simple.json?text=' + req.params.q)
+    .then(parsingToJSON)
+    .then(diceTransformation);
+
+  Promise.all([github, dice]).then(function(results) {
+    console.log("all the files were created");
+    console.log(results);
   });
-
-
-  // request('http://service.dice.com/api/rest/jobsearch/v1/simple.json?text=' + req.params.q, function (error, response, body) 
-  // {
-  //   if (!error && response.statusCode == 200) 
-  //   {
-  //     var result = JSON.parse(body);
-  //     // res.send(result.resultItemList);
-  //     var newArray1 = searchResultsArray.concat(result.resultItemList);
-  //   }
-  // });
-
-  // function github(searchResultsArray) {
-  //   // request
-  //     if (!error && response.statusCode == 200) {
-  //       // concat array
-  //       sendRequest(searchResultsArray);
-  //     }
-  // }
-
-  // function sendRequest(searchResultsArray) {
-  //   res.send(newArray1);
-  // }
 });
 
 // Application
