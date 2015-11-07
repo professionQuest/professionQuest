6bd99f6b8436d13607a0fe8258a6b124708f24a4
Emiluz <emiluzlopez@yahoo.com>
2015-11-06 21:36:59 -0800

    changed stylesheet, views, loging and layout

--- public/javascripts/views.js
+++ public/javascripts/views.js
@@ -2,10 +2,10 @@ var Views = (function(){
 
 	var NewSearchView = Backbone.View.extend({
 		render : function () {
-			var queryInput = '<div class="container"><input id="query" type="text" class="form-group" placeholder="Job Title" aria-describedby="sizing-addon1" value="javascript">';
+			var queryInput = '<div class="container" class="col-md-3"><input id="query" type="text" class="form-group" placeholder="Job Title" aria-describedby="sizing-addon1" value="javascript">';
 			var cityInput = '<input id="city" type="text" class="form-group" placeholder="City or zip code" aria-describedby="sizing-addon1" value="denver, co">';
-			var buttonSearch = '<button id="buttonSearch">Seach</button></div>';
-			var tableJobs = '<div class="container" id="tableTop"><div class="col-md-3">Job Title</div><div class="col-md-3">Company</div><div class="col-md-3">Date</div><div class="col-md-3">Location</div></div>';
+			var buttonSearch = '<input id="buttonSearch" type="submit" class="form-group"></input></div>';
+			var tableJobs = '<div class="container" id="tableTop"><div class="col-sm-3"></div><div class="col-sm-3"></div><div class="col-sm-3"></div><div class="col-sm-3"></div></div>';
 			this.$el.html(queryInput + cityInput + buttonSearch + tableJobs);
 
 			return this;
@@ -26,13 +26,13 @@ var Views = (function(){
 	var SearchResultView = Backbone.View.extend({
 		className : 'search-result',
 		render : function () {
-			var title = '<div class="container"><div class="col-md-3"><a target="_blank" href="' + this.model.get('linkToSource') + '" class="title">' + this.model.get('title') + '</a></div>';
-			var company = '<div class="col-md-3" class="company">' + this.model.get('company') + '</div>';
+			var title = '<div class="container"><div class="col-md-15"><a target="_blank" href="' + this.model.get('linkToSource') + '" class="title">' + this.model.get('title') + '</a></div>';
+			var company = '<div class="col-md-15" class="company">' + this.model.get('company') + '</div>';
 			var convertedDate = new Date(this.model.get('postDate') * 1000);
 			var formattedDate =  convertedDate.getMonth() + '/' + convertedDate.getDate() + '/' + convertedDate.getFullYear();
-			var date = '<div class="col-md-3" class="date">' + formattedDate + '</div>';
-			var location = '<div class="col-md-3" class="location">' + this.model.get('location') + '</div>'
-			var addButton = '<button id="addButton">+</div></div>'
+			var date = '<div class="col-md-15" class="date">' + formattedDate + '</div>';
+			var location = '<div class="col-md-15" class="location">' + this.model.get('location') + '</div>'
+			var addButton = '<button id="addButton" class="submit">save job</button></div>'
 
 			this.$el.html(title + company + date + location + addButton);
 			return this;
