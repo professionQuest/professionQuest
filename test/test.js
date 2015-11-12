c2669ae361024154e8fb7ac45e14e205b89df5a7
David Woolf <davidjohnwoolf@gmail.com>
2015-11-11 19:18:58 -0800

    setup structure for route tests and install mocha and chai

---
+++ test/test.js
@@ -0,0 +1,52 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+// Routes
+
+describe('Application Routes', function() {
+  describe('#application', function() {
+
+  });
+  describe('#request', function() {
+
+  });
+});
+
+describe('Jobs Routes', function() {
+  describe('#index', function() {
+
+  });
+  describe('#new', function() {
+
+  });
+  describe('#destroy', function() {
+
+  });
+});
+
+describe('Sessions Routes', function() {
+  describe('#login', function() {
+
+  });
+  describe('#logout', function() {
+
+  });
+});
+
+describe('Users Routes', function() {
+  describe('#new', function() {
+
+  });
+  describe('#create', function() {
+
+  });
+  describe('#edit', function() {
+
+  });
+  describe('#update', function() {
+
+  });
+  describe('#destroy', function() {
+
+  });
+});
